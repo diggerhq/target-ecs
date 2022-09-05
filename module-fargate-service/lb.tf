13874bf2eacff5e4f2af8fc4c1907922d8f4a0d2
alexey <alexey@digger.dev>
2022-09-05 16:51:42 +0100

    remove unused resources

--- module-fargate-service/lb.tf
+++ module-fargate-service/lb.tf
@@ -9,7 +9,7 @@ data aws_alb "main" {
 }
 
 resource "aws_alb_target_group" "main" {
-  name                 = "${var.service_name}"
+  name                 = var.service_name
   port                 = var.lb_port
   protocol             = var.lb_protocol
   vpc_id               = var.service_vpc.id
@@ -32,75 +32,6 @@ resource "aws_alb_target_group" "main" {
   }
 
   tags = var.tags
-}
-
-data "aws_elb_service_account" "main" {
-}
-
-# bucket for storing ALB access logs
-resource "aws_s3_bucket" "lb_access_logs" {
-  bucket_prefix = "${var.service_name}"
-  tags          = var.tags
-  force_destroy = true
-}
-
-resource "aws_s3_bucket_acl" "lb_access_logs_acl" {
-  bucket = aws_s3_bucket.lb_access_logs.id
-  acl    = "private"
-}
-
-resource "aws_s3_bucket_lifecycle_configuration" "lb_access_logs_lifecycle_rule" {
-  bucket = aws_s3_bucket.lb_access_logs.id
-
-  rule {
-    id     = "cleanup"
-    status = "Enabled"
-
-    abort_incomplete_multipart_upload {
-      days_after_initiation = 1
-    }
-    expiration {
-      days = var.lb_access_logs_expiration_days
-    }
-  }
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "lb_access_logs_server_side_encryption" {
-  bucket = aws_s3_bucket.lb_access_logs.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
-
-# give load balancing service access to the bucket
-resource "aws_s3_bucket_policy" "lb_access_logs" {
-  bucket = aws_s3_bucket.lb_access_logs.id
-
-  policy = <<POLICY
-{
-  "Id": "Policy",
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "s3:PutObject"
-      ],
-      "Effect": "Allow",
-      "Resource": [
-        "${aws_s3_bucket.lb_access_logs.arn}",
-        "${aws_s3_bucket.lb_access_logs.arn}/*"
-      ],
-      "Principal": {
-        "AWS": [ "${data.aws_elb_service_account.main.arn}" ]
-      }
-    }
-  ]
-}
-POLICY
 }
 
 # The load balancer DNS name
